e852cdbd9c7d2da6cc2a07d629f13199e11c001a
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-26 20:10:02 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,10 +7,10 @@ class Anagram
     @word
   end
 
-  def match(anagrams)
-    if true           # @word or self.word check if self[0][0] == @word
-      []
-    end
+  def match(anagrams)  #array ["something", "something else", "even more", "Hi there"]
+    if true           # @word or self.word check if self[0][0] == @word[0], then check if self[0][1] == @word[1], and so on until end of @word.length
+      []              # next is self[1][0] == @word[0], then check self[1][1] == @word[1], and so on...
+    end               # repeat for evey element in anagrams
   end
 
 end
