d29abc9b4d5794734f132f134cdfab85de7d6e3e
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-26 20:15:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,8 +10,8 @@ class Anagram
   def match(anagrams)
     anagrams.each do |letters|
       @word.uniq.sort == letters.uniq.sort                           # do they have the same letters anagrams[i] and @word
-      []
     end
+    []
   end
 
 end
